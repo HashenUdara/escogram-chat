7701f02ee85e120e4e7398fd6e3b784346218e64
Hashen Udara <106765093+HashenUdara@users.noreply.github.com>
2024-06-09 02:12:45 +0530

    Update page.tsx

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -24,7 +24,9 @@ const page = async ({}) => {
         -1
       )) as string[];
 
-      const lastMessage = JSON.parse(lastMessageRaw) as Message;
+      const lastMessage = lastMessageRaw
+        ? (JSON.parse(lastMessageRaw) as Message)
+        : null;
 
       return {
         ...friend,
@@ -34,54 +36,54 @@ const page = async ({}) => {
   );
 
   return (
-    // <div className="container py-8 ">
-    //   <h1 className="font-semibold text-2xl mb-8">Recent Chats</h1>
-    //   {friendsWithLastMessage.length === 0 ? (
-    //     <p className="text-sm text-zinc-500">Nothing to show here...</p>
-    //   ) : (
-    //     friendsWithLastMessage.map((friend) => (
-    //       <div
-    //         key={friend.id}
-    //         className="relative  flex hover:bg-muted/90 bg- p-3 rounded-xl"
-    //       >
-    //         {/* <div className="absolute right-4 inset-y-0 flex items-center">
-    //           <ChevronRight className="h-7 w-7  text-muted-foreground" />
-    //         </div> */}
+    <div className="container py-8 ">
+      <h1 className="font-semibold text-2xl mb-8">Recent Chats</h1>
+      {friendsWithLastMessage.length === 0 ? (
+        <p className="text-sm text-zinc-500">Nothing to show here...</p>
+      ) : (
+        friendsWithLastMessage.map((friend) => (
+          <div
+            key={friend.id}
+            className="relative  flex hover:bg-muted/90 bg- p-3 rounded-xl"
+          >
+            {/* <div className="absolute right-4 inset-y-0 flex items-center">
+              <ChevronRight className="h-7 w-7  text-muted-foreground" />
+            </div> */}
 
-    //         <Link
-    //           href={`/dashboard/chat/${chatHrefConstructor(
-    //             session.user.id,
-    //             friend.id
-    //           )}`}
-    //           className="relative flex"
-    //         >
-    //           <div className="mb-4 flex items-center  flex-shrink-0 sm:mb-0 mr-4">
-    //             <Avatar className=" size-12 flex">
-    //               <AvatarImage
-    //                 src={friend.image!}
-    //                 alt={`${friend.name} profile picture`}
-    //               />
-    //               <AvatarFallback>{getInitials(friend.name!)}</AvatarFallback>
-    //             </Avatar>
-    //           </div>
+            <Link
+              href={`/dashboard/chat/${chatHrefConstructor(
+                session.user.id,
+                friend.id
+              )}`}
+              className="relative flex"
+            >
+              <div className="mb-4 flex items-center  flex-shrink-0 sm:mb-0 mr-4">
+                <Avatar className=" size-12 flex">
+                  <AvatarImage
+                    src={friend.image!}
+                    alt={`${friend.name} profile picture`}
+                  />
+                  <AvatarFallback>{getInitials(friend.name!)}</AvatarFallback>
+                </Avatar>
+              </div>
 
-    //           <div>
-    //             <h4 className="text-lg font-semibold">{friend.name}</h4>
-    //             <p className="mt-1 max-w-md text-md">
-    //               <span className=" text-muted-foreground">
-    //                 {friend.lastMessage.senderId === session.user.id
-    //                   ? "You: "
-    //                   : ""}
-    //               </span>
-    //               {friend.lastMessage.text}
-    //             </p>
-    //           </div>
-    //         </Link>
-    //       </div>
-    //     ))
-    //   )}
-    // </div>
-    <div></div>
+              <div>
+                <h4 className="text-lg font-semibold">{friend.name}</h4>
+                <p className="mt-1 max-w-md text-md">
+                  <span className=" text-muted-foreground">
+                    {friend.lastMessage &&
+                    friend.lastMessage.senderId === session.user.id
+                      ? "You: "
+                      : ""}
+                  </span>
+                  {friend.lastMessage && friend.lastMessage.text}
+                </p>
+              </div>
+            </Link>
+          </div>
+        ))
+      )}
+    </div>
   );
 };
 
